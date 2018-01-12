c53dc66055b4632c3f64763eca8f6227ceb32b87
lyxym <674922949@qq.com>
2018-01-12 17:33:36 +0800

    remove nouse import
    remove nouse import

--- pojo/src/main/java/com/ly/bean/util/FileUtils.java
+++ pojo/src/main/java/com/ly/bean/util/FileUtils.java
@@ -2,7 +2,6 @@ package com.ly.bean.util;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
